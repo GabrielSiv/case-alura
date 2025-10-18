9eaae26ad0b1e2a9710c9733dbfc47179eb6ebf6
Gabriel Silvino <gabriel.siv720@gmail.com>
2025-10-18 11:59:39 -0300

    feat: components exportation

--- src/components/Typography/index.ts
+++ src/components/Typography/index.ts
@@ -1,1 +1,5 @@
 export { Heading1 } from "./Elements/h1";
+export { Heading2 } from "./Elements/h2";
+export { Heading3 } from "./Elements/h3";
+export { Paragraph } from "./Elements/p";
+export { LinkText } from "./Elements/a";
