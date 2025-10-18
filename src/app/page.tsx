cd0af3d6ad5ba18406cc9e40094b72296630b8b9
Gabriel Silvino <gabriel.siv720@gmail.com>
2025-10-18 12:38:32 -0300

    fix: typography tokens as css styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="heading-1">Heading1</h1>
+      <h2 className="heading-2">Heading2</h2>
+      <h3 className="heading-3">Heading2</h3>
+      <p className="paragraph">Paragraph</p>
+      <a className="link">Link</a>
+    </main>
   );
 }
--- src/components/Typography/index.ts
+++
@@ -1,5 +0,0 @@
-export { Heading1 } from "./Elements/h1";
-export { Heading2 } from "./Elements/h2";
-export { Heading3 } from "./Elements/h3";
-export { Paragraph } from "./Elements/p";
-export { LinkText } from "./Elements/a";
